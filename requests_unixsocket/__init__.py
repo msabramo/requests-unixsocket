8d692622c9466fcf3d7d4c1c190c7aaf84267c1e
Marc Abramowitz <marc@marc-abramowitz.com>
2014-11-28 09:49:32 -0800

    Tweak monkeypatch code

--- requests_unixsocket/__init__.py
+++ requests_unixsocket/__init__.py
@@ -1,8 +1,9 @@
 import requests
+import sys
 
 from .adapters import UnixAdapter
 
-__all__ = ['monkeypatch', 'Session', 'UnixAdapter']
+__all__ = ['monkeypatch', 'Session']
 
 DEFAULT_SCHEME = 'http+unix://'
 
@@ -16,11 +17,16 @@ class Session(requests.Session):
 class monkeypatch(object):
     def __init__(self, url_scheme=DEFAULT_SCHEME):
         self.session = Session()
+        requests = self._get_global_requests_module()
         self.orig_requests_get = requests.get
         requests.get = self.session.get
+
+    def _get_global_requests_module(self):
+        return sys.modules['requests']
 
     def __enter__(self):
         return self
 
     def __exit__(self, *args):
+        requests = self._get_global_requests_module()
         requests.get = self.orig_requests_get
