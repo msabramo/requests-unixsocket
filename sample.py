49872aaf25bdf5771f04e0095de1c799b8a45429
Marc Abramowitz <abramowi@adobe.com>
2017-02-19 11:37:20 -0800

    Add sample.py
    Example of using lib to get info from `/var/run/docker.sock`

---
+++ sample.py
@@ -0,0 +1,9 @@
+import json
+
+import requests_unixsocket
+
+session = requests_unixsocket.Session()
+
+r = session.get('http+unix://%2Fvar%2Frun%2Fdocker.sock/info')
+registry_config = r.json()['RegistryConfig']
+print(json.dumps(registry_config, indent=4))
